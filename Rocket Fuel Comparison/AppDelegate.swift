608694ff6d46e89fb9b066ff5c21584ea0477145
Sam <samgauck@icloud.com>
2019-03-04 18:55:44 -0500

    Initial Commit

---
+++ Rocket Fuel Comparison/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Rocket Fuel Comparison
+//
+//  Created by Samasaur1 on 3/4/19.
+//  Copyright © 2019 Samasaur1. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Rocket Fuel Comparison/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Rocket Fuel Comparison
+//
+//  Created by Samasaur1 on 3/4/19.
+//  Copyright © 2019 Samasaur1. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
